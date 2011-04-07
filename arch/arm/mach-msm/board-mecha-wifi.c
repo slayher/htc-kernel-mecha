b8fb16e8ec86be10e974c823b60fcde0093a7b59
Tony Layher <layhertony@gmail.com>
2011-04-06 23:10:15 -0400

    Mecha:  Really reenable HW_OOB on Mecha

--- arch/arm/mach-msm/board-mecha-wifi.c
+++ arch/arm/mach-msm/board-mecha-wifi.c
@@ -77,7 +77,7 @@ static struct resource mecha_wifi_resources[] = {
 		.name		= "bcm4329_wlan_irq",
 		.start		= MSM_GPIO_TO_INT(MECHA_GPIO_WIFI_IRQ),
 		.end		= MSM_GPIO_TO_INT(MECHA_GPIO_WIFI_IRQ),
-		.flags      = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWEDGE,
+		.flags      = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
 	},
 };
 
